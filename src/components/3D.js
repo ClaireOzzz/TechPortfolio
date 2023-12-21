402c31ba174518e35a7ab91b2de5b8f84fd24292
ClaireOzzz <ohdeartx@gmail.com>
2023-12-21 22:16:58 +0800

    added cauterize& smoke effect

--- src/components/3D.js
+++ src/components/3D.js
@@ -27,16 +27,17 @@ const ThreeD = () => {
                   {contentData.threeD.section.title}
                   <div className="line-mf"></div>
                 </h3>
-                <p className="subtitle-a">
+                {/* <p className="subtitle-a">
                   {contentData.threeD.section.description}
-                </p>
+                </p> */}
               </div>
             </Fade>
           </div>
         </div>
         
-      <p  style={isSmall ? { color:"#9775c2" } : { color:"#9775c2", paddingLeft:'1.8rem'}}   > ENVIRONMENT ASSETS</p>
+      <p style={{ fontWeight:"600",fontSize: "1.2rem",  color:"#9775c2", flex:'0 0 auto', width:"100%", maxWidth: "1100px" }}   > ENVIRONMENT ASSETS</p>
         <div className="col-sm-12">
+          
           <div className="row">
             {contentData.threeD.threeD_items.map((project, index) => (
               <div className="Acol-md-4" key={index}>
@@ -88,7 +89,7 @@ const ThreeD = () => {
           </div>
         </div>
 
-        <p style={isSmall ? { color:"#9775c2" } : { color:"#9775c2", paddingLeft:'1.8rem'}}  >CHARACTER & OBJECT ASSETS</p>
+        <p style={{ fontWeight:"600",fontSize: "1.2rem",  color:"#9775c2", flex:'0 0 auto', width:"100%", maxWidth: "1100px" }} >CHARACTER & OBJECT ASSETS</p>
         <div className="col-sm-12">
           <div className="row">
             {contentData.threeD.threeD_items2.map((project, index) => (
@@ -140,10 +141,49 @@ const ThreeD = () => {
             ))}
           </div>
         </div>
-
-        <p style={isSmall ? { color:"#9775c2" } : { color:"#9775c2", paddingLeft:'1.8rem'}}  > SHADERS</p>
-        <div style={isSmall ? {visibility:"visible" } : { color:"#9775c2", paddingLeft:'1.8rem'}}  >
-           <p  > Coming soon...</p>
+        
+        <p style={{ fontWeight:"600",fontSize: "1.2rem",  color:"#9775c2", flex:'0 0 auto', width:"100%", maxWidth: "1100px" }} > SHADERS</p>
+        <div className="col-sm-12">
+          <div className="row">
+            {contentData.threeD.threeD_items3.map((project, index) => (
+              <div className="Acol-md-4" key={index}>
+                <Fade
+                  delay={index * 200}
+                  direction="up"
+                  cascade={false}
+                  triggerOnce={true}
+                >
+                  <div className="card card-recentprojects" >
+                    <div className="card-body">
+                      <div className="card-category-box"></div>
+                      <a
+                      href={project.imageURL}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="col-6-img-a"
+                    >
+                        
+                        <video autoPlay muted loop  className="card-video"  key={index}>
+                          <source src={project.imageURL} type="video/mp4" />
+                          Your browser does not support the video tag.
+                        </video>
+                    </a>
+                        
+                    </div>
+                    <div className="card-footer">
+                      <div className="post-author">
+                        <span className="author">
+                          {project.name} 
+                        </span>
+                      </div>
+                      <div className="post-date">
+                      </div>
+                    </div>
+                  </div>
+                </Fade>
+              </div>
+            ))}
+          </div>
         </div>
 
       </div>
